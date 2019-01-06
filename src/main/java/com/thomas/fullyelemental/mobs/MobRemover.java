c06f6de6074188428783b6a1fd0e8cc6580fec70
Thomas Wessel <turpleturtle12@gmail.com>
2019-01-05 18:49:14 -0800

    Unrealistic mobs no longer spawn

--- src/main/java/com/thomas/fullyelemental/mobs/MobRemover.java
+++ src/main/java/com/thomas/fullyelemental/mobs/MobRemover.java
@@ -1,19 +1,120 @@
 package com.thomas.fullyelemental.mobs;
 
-import net.minecraft.entity.monster.EntitySkeleton;
-import net.minecraft.entity.monster.EntitySpider;
-import net.minecraft.entity.monster.EntityZombie;
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.boss.*;
+import net.minecraft.entity.monster.*;
+import net.minecraft.entity.passive.*;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.text.TextComponentString;
 import net.minecraftforge.event.*;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+
+import org.apache.logging.log4j.Logger;
+
 public class MobRemover{
 	@SubscribeEvent
-	public void onEntitySpawn(EntityJoinWorldEvent event)
-	{
-		if(event.getEntity() instanceof EntitySkeleton || event.getEntity() instanceof EntityZombie || event.getEntity() instanceof EntitySpider) {
+	public void onEntitySpawn(EntityJoinWorldEvent event){
+		
+		if(event.getEntity() instanceof EntityMooshroom) {
 	    	event.setCanceled(true);
 	    }
-	}
+		
+		if(event.getEntity() instanceof EntitySkeletonHorse) {
+	    	event.setCanceled(true);	
+		}
 	
+		if(event.getEntity() instanceof EntityEnderman) {
+	    	event.setCanceled(true);
+		}
+		
+		if(event.getEntity() instanceof EntityPigZombie) {
+	    	event.setCanceled(true);
+		}
+		
+		if(event.getEntity() instanceof EntityBlaze) {
+	    	event.setCanceled(true);
+		}
+		
+		if(event.getEntity() instanceof EntityCreeper) {
+	    	event.setCanceled(true);
+		}
+		
+		if(event.getEntity() instanceof EntityElderGuardian) {
+	    	event.setCanceled(true);
+		}
+		
+		if(event.getEntity() instanceof EntityEndermite) {
+	    	event.setCanceled(true);
+		}
+		
+		if(event.getEntity() instanceof EntityEvoker) {
+	    	event.setCanceled(true);
+		}
+		
+		if(event.getEntity() instanceof EntityGhast) {
+	    	event.setCanceled(true);
+		}
+		
+		if(event.getEntity() instanceof EntityGuardian) {
+	    	event.setCanceled(true);
+		}
+		
+		if(event.getEntity() instanceof EntityHusk) {
+	    	event.setCanceled(true);
+		}
+		
+		if(event.getEntity() instanceof EntityMagmaCube) {
+	    	event.setCanceled(true);
+		}
+		
+		if(event.getEntity() instanceof EntityShulker) {
+	    	event.setCanceled(true);
+		}
+		
+		if(event.getEntity() instanceof EntitySkeleton) {
+	    	event.setCanceled(true);
+		}
+		
+		if(event.getEntity() instanceof EntitySlime) {
+	    	event.setCanceled(true);
+		}
+		
+		if(event.getEntity() instanceof EntityStray) {
+	    	event.setCanceled(true);
+		}
+		
+		if(event.getEntity() instanceof EntityVex) {
+	    	event.setCanceled(true);
+		}
+		
+		if(event.getEntity() instanceof EntityVindicator) {
+	    	event.setCanceled(true);
+		}
+		
+		if(event.getEntity() instanceof EntityWitherSkeleton) {
+	    	event.setCanceled(true);
+		}
+		
+		if(event.getEntity() instanceof EntityZombie) {
+	    	event.setCanceled(true);
+	    	
+	    	logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+		}
+		
+		if(event.getEntity() instanceof EntityIronGolem) {
+	    	event.setCanceled(true);
+		}
+		
+		if(event.getEntity() instanceof EntitySnowman) {
+	    	event.setCanceled(true);
+		}
+		
+		if(event.getEntity() instanceof EntityWither) {
+	    	event.setCanceled(true);
+		}
+			
+	}
+		
 }
